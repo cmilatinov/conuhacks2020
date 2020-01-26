db4145a5f0b99e7ec52f97de985cdb9c8ac80fb6
Cristian Milatinov <cmilatinov@hotmail.com>
2020-01-26 02:16:44 -0500

    app nav

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,26 @@
 <template>
-    <div id="app">
-        <keep-alive>
-            <router-view/>
-        </keep-alive>
+    <div class="full">
+        <div class="nav" v-if="$store.getters.isAuthenticated">
+            <div class="header">
+                <div class="logo-container">
+                    <icon icon="hands-helping" class="logo"/>
+                </div>
+                <div class="title">Vault</div>
+            </div>
+
+            <div class="nav-item" :class="{ active: $route.name === option.route }" :key="option.name" v-for="option in options" @click="_ => $router.push(option.route)">
+                <icon :icon="option.icon" class="nav-item-icon"></icon>
+                <div class="nav-item-title">{{option.name}}</div>
+            </div>
+        </div>
+        <div class="content">
+            <!-- <keep-alive>
+                <router-view/>
+            </keep-alive> -->
+        </div>
+        <div class="profile" v-if="$store.getters.isAuthenticated">
+
+        </div>
     </div>
 </template>
 
@@ -18,19 +36,15 @@
             return {
                 options: [
                     {
-                        name: 'Home',
-                        icon: {
-                            name: 'home'
-                        },
+                        name: 'Dashboard',
+                        icon: 'bars',
                         route: 'home'
                     },
                     {
-                        name: 'Profile',
-                        icon: {
-                            name: 'user'
-                        },
-                        route: 'profile'
-                    }
+                        name: 'Event Creation',
+                        icon: 'pencil-alt',
+                        route: 'event-creation'
+                    },
                 ],
             }
         },
@@ -39,4 +53,125 @@
 
 <style lang="scss">
     @import 'assets/styles/global.scss';
+
+    .full {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        background-color: #F6F4FC;
+    }
+
+    .nav {
+        display: flex;
+        flex-direction: column;
+        align-items: stretch;
+        width: 250px;
+        height: 100%;
+        background-color: #FEFEFE;
+        border-right: 1px solid #E5E7EB;
+        overflow: hidden;
+        
+        .header {
+            display: flex;
+            flex: 0;
+            flex-direction: row;
+            align-items: center;
+            align-self: center;
+            margin: 40px 20px 40px 20px;
+
+            .logo-container {
+                position: relative;
+                width: 70px;
+                height: 70px;
+                border-radius: 15px;
+                background-color: rgba(0,20,50,0.6);
+                margin-right: 20px;
+                
+                .logo {
+                    position: absolute;
+                    width: 100%;
+                    height: 100%;
+                    display: flex;
+                    justify-content: center;
+                    align-items: center;
+                    padding: 10px;
+                    color: #FEFEFE;
+                }
+
+            }
+
+            .title {
+                flex: 1;
+                font-size: 1.4em;
+                font-weight: 600;
+            }
+
+        }
+        
+        .nav-item {
+            position: relative;
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            margin: 20px 0;
+            opacity: 0.4;
+            transition: 0.2s;
+            cursor: pointer;
+            
+            &:hover {
+                opacity: 0.6;
+            }
+
+            &.active {
+                opacity: 1;
+            }
+
+            &.active ::after {
+                content: "";
+                display: inline-block;
+                position: absolute;
+                right: -3px;
+                top: 1px;
+                width: 3px;
+                height: 100%;
+                background-color: #ffc107;
+                transition: 0.2s;
+            }
+
+            &.active .nav-item-icon {
+                transform: translateX(3px);
+            }
+
+            &.active .nav-item-title {
+                transform: translateX(-3px);
+            }
+
+            .nav-item-icon {
+                margin-left: 40px;
+                margin-right: 25px;
+                transition: 0.2s;
+            }
+
+            .nav-item-title {
+                padding: 5px 20px 5px 0;
+                font-weight: 600;
+                flex: 1;
+                text-align: center;
+                transition: 0.2s;
+            }
+
+        }
+    }
+
+    .content {
+        flex: 1;
+    }
+
+    .profile {
+        width: 350px;
+        height: 100%;
+        background-color: #FEFEFE;
+        border-left: 1px solid #E5E7EB;
+    }
 </style>
